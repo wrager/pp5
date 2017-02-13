5e7924a6e5509d13682f097a0593ba53c6811759
Bochkarev Maxim <qontrol1488@gmail.com>
2017-02-14 01:08:35 +0300

    Added console parametres read

--- lw5/lw5/lw5.cpp
+++ lw5/lw5/lw5.cpp
@@ -3,13 +3,42 @@
 
 using namespace std;
 
+bool ReadCommandLineParams(int argc, char *argv[], int& clientsCount);
+
 int main(int argc, char *argv[])
 {
-	int iterNum = 1000000;
+	int iterNum = 0;
+
+	if (!ReadCommandLineParams(argc, argv, iterNum))
+	{
+		cout << "Command line parametres read error. Run app with /? to help.\n";
+		return ERR;
+	}
 
 	srand(time(NULL));
 	cout << CMonteCarloPiCalculator::GetPi(iterNum) << endl;
 	getchar();
 
-    return SUCCESS;
+	return SUCCESS;
+}
+
+bool ReadCommandLineParams(int argc, char *argv[], int& iterNum)
+{
+	if (argc != 2)
+	{
+		cout << "Incorrect amount of parametres. Use /? for help. " << endl;
+		return false;
+	}
+	else
+	{
+		try {
+			iterNum = atoi(argv[1]);
+			return true;
+		}
+		catch (exception e) {
+			cout << "Command line parse error, check your input!\n";
+			return false;
+		}
+	}
+	return true;
 }

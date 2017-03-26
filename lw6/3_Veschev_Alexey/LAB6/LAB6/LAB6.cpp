a96a87a061a797f8d49057b0a1c228d3b01d907f
ultimatum424 <Good20032014>
2017-03-26 14:15:08 +0300

    Implemented basic encryption functions - Caesar encryption - XOR with password encryption

---
+++ lw6/3_Veschev_Alexey/LAB6/LAB6/LAB6.cpp
@@ -0,0 +1,125 @@
+// LAB6.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <map>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <vector>
+
+namespace
+{
+	std::map<char, int> ReadAlphabetFormFile(const std::string fileName)
+	{
+		typedef std::istreambuf_iterator<char> buf_iter;
+		std::fstream file(fileName);
+		std::map<char, int> alphabet;
+		int count = 1;
+		for (buf_iter i(file), e; i != e; ++i) 
+		{
+			char c = *i;
+			alphabet.insert(std::make_pair(c, count++));
+		}
+	
+		return alphabet;
+	}
+
+	std::vector<char> ReadMessageFromFile(const std::string fileName)
+	{
+		typedef std::istreambuf_iterator<char> buf_iter;
+		std::fstream file(fileName);
+		std::vector<char> mass;
+		for (buf_iter i(file), e; i != e; ++i)
+		{
+			char c = *i;
+			mass.push_back(c);
+		}
+
+		return mass;
+	}
+
+	std::vector<int> ConvertCharToInt(const std::vector<char> & message, std::map<char, int> & alphabet)
+	{
+		std::vector<int> messageCode;
+		for (char symbol : message)
+		{
+			int codeSymbol = alphabet[symbol];
+			if (codeSymbol)
+			{
+				messageCode.push_back(codeSymbol);
+			}
+		}
+		return  messageCode;
+	}
+	
+	std::vector<char> ConvertIntToChar(const std::vector<int> messageCode, std::map<char, int> & alphabet)
+	{
+		std::map<char, int>::const_iterator it;
+		int key = 0;
+		std::vector<char> message;
+		for (int code : messageCode)
+		{
+			for (it = alphabet.begin(); it != alphabet.end(); it++)
+			{
+				if (it->second == code)
+				{
+					key = it->first;
+					message.push_back(key);
+					break;
+				}
+			}
+		}
+		return  message;
+	}
+
+	void СodingForGamma(std::vector<char> & message, std::map<char, int> alphabet, std::string gamma)
+	{
+		int sizeAlphabet = alphabet.size();
+		std::vector<int> messageCode;
+		for (char line : message)
+		{
+			messageCode.push_back(alphabet[line]);
+		}
+		for (int i = 0; i < message.size(); i++)
+		{
+			int iGamma = alphabet[gamma[i % gamma.size()]];
+			messageCode[i] = (iGamma + messageCode[i]) % sizeAlphabet;
+		}
+	}
+
+	void CondingForCaesar(std::vector<char> & message, std::map<char, int> alphabet, int key)
+	{
+		int sizeAlphabet = alphabet.size();
+		std::vector<int> messageCode;
+		for (char line : message)
+		{
+			messageCode.push_back(alphabet[line]);
+		}
+
+		for (int i = 0; i < message.size(); i++)
+		{
+			messageCode[i] = (key + messageCode[i]) % sizeAlphabet;
+		}
+	}
+}
+
+int main()
+{
+	std::string gamma;
+	std::map<char, int> alphabet = ReadAlphabetFormFile("alpha_eng.txt");
+	std::vector<char> message = ReadMessageFromFile("file.txt");
+	std::vector<int> code = ConvertCharToInt(message, alphabet);
+	message = ConvertIntToChar(code, alphabet);
+	//std::cin >> gamma;
+	std::ofstream outFile("out.txt");
+	for (auto i : alphabet)
+	{
+		outFile.put(i.first);
+	}
+	std::cout << std::endl;
+	std::cout << alphabet.size();
+	std::vector<int> messageCode;
+    return 0;
+}
+

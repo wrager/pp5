c60b231be2531bbc12b6c28ccc725513767d386a
Alexander <www.darmagh@mail.ru>
2017-04-02 14:22:56 +0300

    added merge sort

--- Chibirev Alexander/lab6/lab6/lab6/lab6.cpp
+++ Chibirev Alexander/lab6/lab6/lab6/lab6.cpp
@@ -2,10 +2,22 @@
 //
 
 #include "stdafx.h"
-
+#include "CMergeSort.h"
+#include "CSorterManager.h"
 
 int main()
 {
-    return 0;
+	try
+	{
+		std::cout << "Simple merge sort:" << "\n";
+		CMergeSort mergeCalc;
+		CSorterManager mergeSorter(mergeCalc, "data.txt");
+		mergeSorter.Start();
+	}
+	catch (const std::exception & except)
+	{
+		std::cout << except.what() << std::endl;
+		return 1;
+	}
+	return 0;
 }
-

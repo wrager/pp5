ebd4cae036546abe3ef168b4f2dde9a5c0421ba0
Oleg Anikin <holyrecycler@mail.ru>
2017-04-14 12:29:09 +0300

    Lw6 Anikin Oleg (2)
    * создал проект, добавил сортировки и интерфейс сортировщика
    
    * отказался от идеи шаблонов, начал реализовывать через агрегаторы
    
    * исправил опечатку в имени классов, первая рабочая версия
    
    * добавил агрегатора, рефакторинг
    
    * выделил базовый калькулятор, реализовал шелла, навел красоты
    
    * переименовал папку
    
    * добавил шаблонный метод

---
+++ lw6/2_Anikin_Oleg/ArraySort/BaseSortCalculator.cpp
@@ -0,0 +1,30 @@
+#include "stdafx.h"
+#include "BaseSortCalculator.h"
+
+CBaseSortCalculator::CBaseSortCalculator(const std::vector<int>& vec)
+    : m_array(vec)
+    , m_isParallelEnabled(false)
+{
+}
+
+void CBaseSortCalculator::WaitForMultiplyObjects(std::vector<std::thread>& threads)
+{
+    std::for_each(threads.begin(), threads.end(), [](std::thread& thread) { thread.join(); });
+}
+
+
+void CBaseSortCalculator::Sort(bool isParallel)
+{
+    m_isParallelEnabled = isParallel;
+    SortBehavior();
+}
+
+std::vector<int> CBaseSortCalculator::GetResult() const
+{
+    return m_array;
+}
+
+void CBaseSortCalculator::Reset(const std::vector<int>& array)
+{
+    m_array = array;
+}
---
+++ lw6/2_Anikin_Oleg/ArraySort/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+#include <vector>
+#include <functional>
+#include <memory>
+#include <iostream>
+#include <thread>
+#include <algorithm>
+#include <ctime>
+#include <fstream>
+#include <string>

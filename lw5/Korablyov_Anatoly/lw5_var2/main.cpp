b078f942399c114a9284b82e515bf0b680a26b8d
Анатолий Кораблёв < >
2017-05-11 23:27:30 +0300

    read and write to file

--- lw5/Korablyov_Anatoly/lw5_var2/main.cpp
+++ lw5/Korablyov_Anatoly/lw5_var2/main.cpp
@@ -1,25 +1,49 @@
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include <string>
+
 #include "SortBatcher.h"
 
-int main()
+std::vector<unsigned> ReadFile(const std::string & nameFile)
 {
-	std::vector<unsigned> number = { };
+	std::ifstream inpFile(nameFile);
+	if (!inpFile.is_open())
+	{
+		std::cout << "File not found!";
+		exit(1);
+	}
+
+	std::vector<unsigned> vecNum;
+	while (!inpFile.eof())
+	{
+		std::string mainString;
+		inpFile >> mainString;
+		vecNum.push_back(atoi(mainString.c_str()));
+	}
 	
-	std::cout << "{ ";
-	for (auto it : number)
-	{
-		std::cout << it << " ";
-	}
-	std::cout << "}" << std::endl;
+	return vecNum;
+}
 
-	CSortBatcher sortBatch;
-	std::vector<unsigned> res = sortBatch.sortBatcher(number);
-
-	std::cout << "{ ";
-	for (auto it : res)
+void WriteToFile(const std::string & nameFile, const std::vector<unsigned> & numArr)
+{
+	std::ofstream inpFile(nameFile);
+	inpFile << "{ ";
+	for (auto it : numArr)
 	{
-		std::cout << it << " ";
+		inpFile << it << ", ";
 	}
-	std::cout << "}" << std::endl;
+	inpFile << " }";
+}
+
+int main(int argc, char * argv[])
+{
+	if (argc == 3)
+	{
+		std::vector<unsigned> number = ReadFile(argv[1]);
+		CSortBatcher sortBatch;
+		std::vector<unsigned> res = sortBatch.sortBatcher(number);
+		WriteToFile(argv[2], res);
+	}
+	return 0;
 }

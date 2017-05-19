1359fcc3a87b7d61d619f5c7a6e0c37d79952c91
Smi1le <tima55510@gmail.com>
2017-05-19 17:19:06 +0300

    add class IOManager.cs and him realization

---
+++ lw7/Garanin Timofey/DictionaryCompressor/DictionaryCompressor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryCompressor
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0]);
+            Console.WriteLine(args[1]);
+            IOManager man = new IOManager(args[0], args[1]);
+            man.SettingInputFile();
+            string data = man.GetViewMappingFile();
+            Console.WriteLine(data);
+            Console.WriteLine(man.GetViewMappingFile().Length);
+
+            using (StreamWriter sw = new StreamWriter("out.txt", false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(data);
+            }
+        }
+    }
+}

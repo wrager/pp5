7eb6c3b0b8e80c373108145eb1108143da1f50b0
Biryukov <intmpg@gmail.com>
2017-05-20 20:08:50 +0300

    restruct

---
+++ lw7/Biryukov_Georgy/lw7/ArrayManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+class CArrayManager
+{
+    public static int[] fillArray(string path)
+    {
+        string file = File.ReadAllText(path);
+
+        int[] nums = file
+        .Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+        .Select(n => int.Parse(n))
+        .ToArray();
+
+        return nums;
+    }
+}
---
+++ lw7/Biryukov_Georgy/lw7/ArrayManupulator.cs
@@ -0,0 +1,35 @@
+﻿class CArrayManipulator
+{
+    public static int[] sortingProcess(int[] array)
+    {
+        int gap = array.Length / 2;
+        while (gap > 0)
+        {
+            insertionSorting(array, gap);
+            if (gap == 2)
+            {
+                gap = 1;
+            }
+            else
+            {
+                gap = (int)(gap / 2.2);
+            }
+        }
+        return array;
+    }
+
+    static void insertionSorting(int[] array, int gap)
+    {
+        for (int i = 0; i < array.Length - gap; i++)
+        {
+            int j = i + gap;
+            int tmp = array[j];
+            while (j >= gap && tmp > array[j - gap])
+            {
+                array[j] = array[j - gap];
+                j -= gap;
+            }
+            array[j] = tmp;
+        }
+    }
+}

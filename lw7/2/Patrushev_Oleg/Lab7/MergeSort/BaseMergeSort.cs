b9cd9fce85feab145e19a61c9a0b021db75ca86a
Oleg <patrushev1996@gmail.com>
2017-05-23 00:04:04 +0300

    Added clearly

---
+++ lw7/2/Patrushev_Oleg/Lab7/MergeSort/BaseMergeSort.cs
@@ -0,0 +1,25 @@
+﻿using Lab7.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7.MergeSort
+{
+    abstract class BaseMergeSort : BaseSorter
+    {
+        protected BaseMergeSort(List<int> vector)
+            : base(vector)
+        {
+            
+        }
+
+        public override void SortIteration()
+        {
+            MergeSort(0, _startVectorSize - 1);
+        }
+
+        protected abstract void MergeSort(int p, int r);
+    }
+}

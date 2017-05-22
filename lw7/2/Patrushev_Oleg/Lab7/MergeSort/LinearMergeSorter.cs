6f1bc09ab723d1ff8fc5da7d417758cb911ab4d4
Oleg <patrushev1996@gmail.com>
2017-05-22 23:59:55 +0300

    Added 7 lab

---
+++ lw7/2/Patrushev_Oleg/Lab7/MergeSort/LinearMergeSorter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7.MergeSort
+{
+    class LinearMergeSorter : BaseMergeSort
+    {
+        public LinearMergeSorter(List<int> vector)
+            : base(vector)
+        {
+            
+        }
+
+        protected override void MergeSort(int p, int r)
+        {
+            if (p >= r)
+            {
+                return;
+            }
+
+            int q = (p + r) / 2;
+
+            MergeSort(p, q);
+            MergeSort(q + 1, r);
+            Utils.Merge(ref _vector, p, q, r);
+        }
+    }
+}
---
+++ lw7/2/Patrushev_Oleg/Lab7/ShellSort/ParallelShellSorter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Lab7.ShellSort
+{
+    class ParallelShellSorter : BaseShellSorter
+    {
+        List<Thread> _threads;
+
+        public ParallelShellSorter(List<int> vector)
+            : base(vector)
+        {
+            _threads = new List<Thread>();
+        }
+
+        protected override void ShellSort()
+        {
+            for (int h = _vector.Count / 2; h > 0; h = h / 2)
+            {
+                //Parallel.Invoke(() => ThreadFunction(h));
+                _threads.Add(new Thread(delegate () { ThreadFunction(h); }));
+                _threads.Last().Start();
+            }
+            _threads.ForEach(thread => thread.Join());
+        }
+
+        private void ThreadFunction(int h)
+        {
+            for (int i = 0; i < h; i++)
+            {
+                Utils.InsertSort(ref _vector, i, h);
+            }
+        }
+    }
+}

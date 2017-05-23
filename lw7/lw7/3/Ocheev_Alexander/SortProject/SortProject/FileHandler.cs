8f2641a1fc5cb336726745149a0704274bb4668e
SashaOcheev <OcheevSasha@gmail.com>
2017-05-23 08:48:53 +0300

    file handler

---
+++ lw7/lw7/3/Ocheev_Alexander/SortProject/SortProject/FileHandler.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortProject.Sorter
+{
+    public class FileHandler
+    {
+        public void ReadInput(string inputFile)
+        {
+            var numbers = File.ReadAllText( inputFile )
+                .Split( ' ' )
+                .Where( str => str != " " )
+                .Select( str => int.Parse( str ) );
+
+            _list = numbers.ToList();
+        }
+
+        private void WriteOutput(string outputFile)
+        {
+            using ( System.IO.StreamWriter file = new System.IO.StreamWriter( outputFile, false ) )
+            {
+                foreach ( var number in _list )
+                {
+                    file.Write( number.ToString() + " " );
+                }
+            }
+        }
+
+        public void Run(string text, string outputFile)
+        {
+            Sorter.Lst = _list;
+            Sorter.Sort();
+
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            watch.Stop();
+
+            Sorter.Sort();
+
+            var elapsedMs = watch.ElapsedMilliseconds;
+
+            Console.WriteLine( string.Format( "{0} time: {1}", text, elapsedMs.ToString() ) );
+        }
+
+        public AbstractSorter<int> Sorter { set; private get; }
+
+        List<int> _list;
+    }
+}
--- lw7/lw7/3/Ocheev_Alexander/SortProject/SortProject/Program.cs
+++ lw7/lw7/3/Ocheev_Alexander/SortProject/SortProject/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using SortProject.Sorter;
+using SortProject.Sorter.MergerSorter;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,14 @@ namespace SortProject
     {
         static void Main( string[] args )
         {
+            var handler = new FileHandler();
+            handler.ReadInput( args[ 0 ] );
+
+            handler.Sorter = new LinearMergeSorter<int>();
+            handler.Run( "Linear", "linear_merge.txt" );
+
+            handler.Sorter = new ParallelMergeSorter<int>();
+            handler.Run( "Parallel", "parallel_merge.txt" );
         }
     }
 }

1e9ad415626b05e2ec6f80a3fbd055e437662532
ultimatum424 <Good20032014>
2017-05-25 22:08:05 +0300

    add line version

---
+++ lw7/3_Veschev_Alexey/LAB7/CWorker.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LAB7
+{
+    class CWorker
+    {
+        public List<char> ReadFormFile(string fileName)
+        {
+            List<char> message = new List<char>();
+            StreamReader fReader = null;
+            try
+            {
+                string startupPatch = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                fReader = new StreamReader(startupPatch + "/tests/" + fileName);
+                message = new List<char>(fReader.ReadToEnd());
+                return message;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                fReader.Close();
+            }
+
+        }
+
+        public void WriteMessageToFile(List<char> message, string fileStr)
+        {
+            StreamWriter fWriter = null;
+            try
+            {
+                string startupPatch = Directory.GetParent(Directory.GetCurrentDirectory()).Parent?.FullName;
+                fWriter = new StreamWriter(startupPatch + "/tests/" + fileStr);
+                fWriter.Write(message.ToArray());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            finally
+            {
+                fWriter?.Close();
+            }
+        }
+
+        public void EncipherCaesar(string alphabetFile, string fileStr, int key)
+        {
+            CAlphabet alphabet = new CAlphabet();
+            alphabet.ReadFormFile(alphabetFile);
+            List<char> messageForLine = ReadFormFile(fileStr);
+            List<char> messageForThread = messageForLine;
+            //Line
+            CLineCrypto lineCrypto = new CLineCrypto(messageForLine, alphabet);
+            Console.WriteLine("Start Encipher Caesar");
+            Stopwatch timer = Stopwatch.StartNew();
+            lineCrypto.EncipherCaesar(key);
+            long elapsedTimeForLine = timer.ElapsedMilliseconds;
+            Console.Write(elapsedTimeForLine.ToString());
+            Console.WriteLine(" ms. for line");
+            WriteMessageToFile(messageForLine, "out_line_Caesar.txt");
+            //Thread
+        }
+    }
+}
---
+++ lw7/3_Veschev_Alexey/LAB7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CWorker worker = new CWorker();
+            List<char> message = worker.ReadFormFile("message.txt");
+            worker.EncipherCaesar("alphabet_eng.txt", "Harry_Potter_1_for_time.txt", 5);
+            //worker.EncipherCaesar("alphabet_eng.txt", "message.txt", 5);
+        }
+    }
+}

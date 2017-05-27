8cb3f772ba55c78461cb88fd243b0692a07f8d7d
Oleg Anikin <holyrecycler@mail.ru>
2017-05-27 23:50:59 +0300

    Anikin Oleg lw7 (2)

---
+++ lw7/2/Anikin_Oleg/ArraySort/ArraySort/ShellSortCalculator.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+
+namespace ArraySort
+{
+    class ShellSortCalculator : BaseCalculator
+    {
+        public ShellSortCalculator(List<int> vec) : base(vec)
+        {
+        }
+
+        private void IterationFunc(object endObj)
+        {
+            int end = (int)endObj;
+            for (int i = 0; i < end; i++)
+            {
+                ShellIteration(i, end);
+            }
+        }
+
+        protected override void SortBehavior()
+        {
+            List<Thread> tasks = new List<Thread>();
+            for (int end = m_array.Count / 2; end > 0; end /= 2)
+            {
+                if (m_isParallelEnabled)
+                {
+                    Thread th = new Thread(new ParameterizedThreadStart(IterationFunc));
+                    th.Start(end);
+                    tasks.Add(th);
+                }
+                else
+                {
+                    IterationFunc(end);
+                }
+            }
+
+            if (m_isParallelEnabled)
+            {
+                foreach(Thread task in tasks)
+                {
+                    task.Join();
+                }
+            }
+        }
+
+        private void ShellIteration(int current, int half)
+        {
+            for (int shifted = half + current, start = shifted; shifted < m_array.Count; shifted += half, start = shifted)
+            {
+                while (start > current && m_array[start - half] > m_array[start])
+                {
+                    int temp = m_array[start];
+                    m_array[start] = m_array[start - half];
+                    m_array[start - half] = temp;
+                    start -= half;
+                }
+            }
+        }
+    }
+}

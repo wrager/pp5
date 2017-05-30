f1198b6c7af661db58e32ace3c4dd5e37cba0a70
Alexey Veschev <ultimatum424@yandex.ru>
2017-05-30 09:11:34 +0300

    Veschev Alexey lw4 (3 + gamma)
    Veschev Alexey lw7 (3 + gamma)

---
+++ lw7/3_Veschev_Alexey/LAB7/CAlphabet.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB7
+{
+    class CAlphabet
+    {
+        private Dictionary<char, int> m_alphabet;
+
+        public CAlphabet()
+        {
+            m_alphabet = new Dictionary<char, int>();
+        }
+
+        public void ReadFormFile(string fileName)
+        {
+            try
+            {
+                string startupPatch = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.FullName;
+                FileStream fstream = File.OpenRead(startupPatch + "/tests/" + fileName);
+                byte[] array = new byte[fstream.Length];
+                fstream.Read(array, 0, array.Length);
+                char[] charArray =  System.Text.Encoding.Default.GetChars(array);
+
+                m_alphabet.Clear();
+                for (int i = 1; i <= charArray.Length; i++)
+                {
+                    m_alphabet.Add(charArray[i - 1], i);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+           
+
+
+        }
+
+        public int GetCodeForSymbol(char symbol)
+        {
+            foreach (var varibale in m_alphabet)
+            {
+                if (varibale.Key == symbol)
+                {
+                    return varibale.Value;
+                }
+            }
+            return -1;
+        }
+
+        public char GetSymbolForCode(int code)
+        {
+            foreach (var varibale in m_alphabet)
+            {
+                if (varibale.Value == code)
+                {
+                    return varibale.Key;
+                }
+            }
+            return '-';
+        }
+
+        public long GetSize()
+        {
+           return m_alphabet.Count;
+        }
+    }
+}

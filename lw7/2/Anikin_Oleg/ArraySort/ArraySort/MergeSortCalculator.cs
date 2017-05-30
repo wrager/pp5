d0b71dbd1bcaa0a28abb208ca9b5ed29c05ad237
Vladimir Pechishev <pechishev.volodya@yandex.ru>
2017-05-30 09:14:00 +0300

    commit revert

---
+++ lw7/2/Anikin_Oleg/ArraySort/ArraySort/MergeSortCalculator.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ArraySort
+{
+    class MergeSortCalculator : BaseCalculator
+    {
+        public MergeSortCalculator(List<int> vec) : base(vec)
+        {
+        }
+
+        protected override void SortBehavior()
+        {
+            StartSort(0, m_array.Count - 1);
+        }
+
+        private void StartSort(int left, int right)
+        {
+            if (right > left)
+            {
+                int middle = (left + right) / 2;
+                if (m_isParallelEnabled && (right - left + 1 == m_array.Count / (Environment.ProcessorCount)))
+                {
+                    Task th1 = Task.Factory.StartNew(() => StartSort(left, middle));
+                    Task th2 = Task.Factory.StartNew(() => StartSort(middle + 1, right));
+                    Task.WaitAll(th1, th2);
+                }
+                else
+                {
+                    StartSort(left, middle);
+                    StartSort(middle + 1, right);
+                }
+                Merge(left, middle, right);
+            }
+        }
+
+        private void Merge(int leftStart, int mid, int rightEnd)
+        {
+            int leftEnd = leftStart;
+            int rightStart = mid + 1;
+
+            List<int> sorted = new List<int>(new int[rightEnd -leftStart + 1]);
+            int cur = 0;
+
+            while (leftEnd <= mid && rightStart <= rightEnd)
+            {
+                if (m_array[leftEnd] <= m_array[rightStart])
+                {
+                    sorted[cur++] = m_array[leftEnd++];
+                }
+                else
+                {
+                    sorted[cur++] = m_array[rightStart++];
+                }
+            }
+
+            while (leftEnd <= mid)
+            {
+                sorted[cur++] = m_array[leftEnd++];
+            }
+            while (rightStart <= rightEnd)
+            {
+                sorted[cur++] = m_array[rightStart++];
+            }
+
+            while (--cur >= 0)
+            {
+                m_array[leftStart + cur] = sorted[cur];
+            }
+        }
+    }
+}

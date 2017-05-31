5a703834f276df7f958eea8faa9ca15589dd75ca
Biryukov <intmpg@gmail.com>
2017-05-31 21:08:50 +0300

    added multithread mergesoring method

--- lw7/Biryukov_Georgy/lw7/ArrayManupulator.cs
+++ lw7/Biryukov_Georgy/lw7/ArrayManupulator.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
 class CArrayManipulator
 {
+
     public static int[] sortingProcess(int[] array)
     {
         int gap = array.Length / 2;
@@ -59,5 +61,84 @@ class CArrayManipulator
             }
             array[j] = tmp;
         }
+    }
+
+    public static void MergeSortParallel(int[] input, int low, int high)
+    {
+
+        if (low < high)
+        {
+            int middle = (low / 2) + (high / 2);
+            Task thread1 = Task.Factory.StartNew(() => MergeSort(input, low, middle));
+            Task thread2 = Task.Factory.StartNew(() => MergeSort(input, middle + 1, high));
+            Task.WaitAll(thread1, thread2);
+            Merge(input, low, middle, high);
+        }
+    }
+
+    public static void MergeSort(int[] input, int low, int high)
+    {
+        if (low < high)
+        {
+            int middle = (low / 2) + (high / 2);
+            MergeSort(input, low, middle);
+            MergeSort(input, middle + 1, high);
+            Merge(input, low, middle, high);
+        }
+    }
+
+    private static void MergeSort(int[] input)
+    {
+        MergeSort(input, 0, input.Length - 1);
+    }
+
+    private static void Merge(int[] input, int low, int middle, int high)
+    {
+
+        int left = low;
+        int right = middle + 1;
+        int[] tmp = new int[(high - low) + 1];
+        int tmpIndex = 0;
+
+        while ((left <= middle) && (right <= high))
+        {
+            if (input[left] < input[right])
+            {
+                tmp[tmpIndex] = input[left];
+                left = left + 1;
+            }
+            else
+            {
+                tmp[tmpIndex] = input[right];
+                right = right + 1;
+            }
+            tmpIndex = tmpIndex + 1;
+        }
+
+        if (left <= middle)
+        {
+            while (left <= middle)
+            {
+                tmp[tmpIndex] = input[left];
+                left = left + 1;
+                tmpIndex = tmpIndex + 1;
+            }
+        }
+
+        if (right <= high)
+        {
+            while (right <= high)
+            {
+                tmp[tmpIndex] = input[right];
+                right = right + 1;
+                tmpIndex = tmpIndex + 1;
+            }
+        }
+
+        for (int i = 0; i < tmp.Length; i++)
+        {
+            input[low + i] = tmp[i];
+        }
+
     }
 }
--- lw7/Biryukov_Georgy/lw7/Program.cs
+++ lw7/Biryukov_Georgy/lw7/Program.cs
@@ -12,21 +12,38 @@ namespace lw7
         {
             string path = args[0];
             int[] arrayForSingleThread = CArrayManager.fillArray(path);
-            int[] arrayForMultiThread = arrayForSingleThread;
+            int[] arrayForMultiThread = CArrayManager.fillArray(path);
+            int[] arrayForParallelMerge = CArrayManager.fillArray(path);
+            int[] arrayForMerge = CArrayManager.fillArray(path);
 
+            Stopwatch sWatch3 = new Stopwatch();
             Stopwatch sWatch = new Stopwatch();
             Stopwatch sWatch2 = new Stopwatch();
+            Stopwatch sWatch4 = new Stopwatch();
 
             sWatch.Start();
             CArrayManipulator.sortingProcess(arrayForSingleThread);
             sWatch.Stop();
+
             sWatch2.Start();
             CArrayManipulator.sortingProcessParallel(arrayForMultiThread);
             sWatch2.Stop();
 
-            CArrayManager.output(arrayForMultiThread);
-            Console.WriteLine("One thread " + sWatch.ElapsedMilliseconds.ToString() + " ms");
-            Console.WriteLine("Multithread " + sWatch2.ElapsedMilliseconds.ToString() + " ms");                
+            sWatch3.Start();
+            CArrayManipulator.MergeSort(arrayForMerge, 0, arrayForMerge.Length - 1); ;
+            sWatch3.Stop();
+
+            sWatch4.Start();
+            CArrayManipulator.MergeSortParallel(arrayForParallelMerge, 0, arrayForParallelMerge.Length - 1); ;
+            sWatch4.Stop();
+
+            Console.WriteLine("Shell one thread " + sWatch.ElapsedMilliseconds.ToString() + " ms");
+            Console.WriteLine("Shell multithread " + sWatch2.ElapsedMilliseconds.ToString() + " ms");
+            Console.WriteLine("Merge one thread " + sWatch3.ElapsedMilliseconds.ToString() + " ms");
+            Console.WriteLine("Merge multithread " + sWatch4.ElapsedMilliseconds.ToString() + " ms");
+
+            CArrayManager.output(arrayForMerge);
+
         }
     }
 }

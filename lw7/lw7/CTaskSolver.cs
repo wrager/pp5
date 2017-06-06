911b262664e3700879e4ad44eee73e4653ccb68f
7kia <il7kia@yandex.ru>
2017-06-06 22:22:24 +0300

    Add 7 lab for C sharp

---
+++ lw7/lw7/CTaskSolver.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace lw7
+{
+    internal class CTaskSolver
+    {
+        private ulong m_numberProcess;
+        private ulong m_amountIterations;
+
+        class ThreadResult
+        {
+            public ThreadResult()
+            {
+
+            }
+
+            public ThreadResult(double res)
+            {
+                result = res;
+            }
+            public double result;
+        };
+
+        static public double GetPi(ulong amountIteration, ulong amountThreads)
+        {
+            double result = 0.0;
+
+            List<Thread> threads = new List<Thread>();
+            List<ThreadResult> threadResults = new List<ThreadResult>();
+
+            for (ulong index = 0; index < amountThreads; ++index)
+            {
+                threadResults.Add(new ThreadResult());
+                int intIndex = Convert.ToInt32(index);
+
+                Thread thread = new Thread(
+                    () =>
+                    {
+                        ComputePi(
+                            index,
+                            amountIteration / amountThreads,
+                            amountIteration,
+                            threadResults[intIndex]
+                        );
+                    }
+                );
+                thread.Start();
+                thread.Join();
+                threads.Add(thread);
+            };
+
+          
+
+            for (int index = 0; index < Convert.ToInt32(amountThreads); ++index)
+            {
+                if (!threads[index].IsAlive)// was joinable
+                {
+                    result += threadResults[index].result;
+                }
+            }
+
+            return result;
+        }
+
+        static private void ComputePi(
+            ulong threadId,
+            ulong amountIteration,
+            ulong amountPoints,
+            ThreadResult result//&
+        )
+        {
+            //srand(UINT(time(NULL) + threadId));
+
+            double resultTheThread = 0.0;
+
+            // Получается(для 4 потоков) 4-верть пи из-за того берём четверть точек
+            // и делим на кол-во итераций
+
+            // один поток береёт четверть точек и вычисляет коэфициент для четверти
+            resultTheThread = 4.0 * CalculateHits(amountIteration) / amountPoints;
+
+            Console.WriteLine(GetMessageForThread(amountIteration, resultTheThread, threadId));
+
+            result.result = resultTheThread;
+        }
+
+        static private double RandomNumber()
+        {
+            Random rand = new Random();
+            return rand.NextDouble();
+        }
+
+        static private bool InCircle(double x, double y)
+        {
+            return (x * x + y * y) <= 1;
+        }
+
+        static private ulong CalculateHits(ulong numIter)
+        {
+            ulong numHits = 0;
+            for (ulong index = 0; index < numIter; ++index)
+            {
+                if(InCircle(RandomNumber(), RandomNumber()))
+                {
+                    ++numHits;
+                }
+            }
+            return numHits;
+        }
+
+        // Evenly distributes indexes on processors
+        static private string GetMessageForThread(
+            ulong amountIteration
+            , double result
+            , ulong indexThread
+        )
+        {
+            string message = "Id thread "
+                + indexThread.ToString() + "\n"
+                + "Amount iteration = " + amountIteration.ToString() + "\n"
+                + "Result = " + result.ToString() + "\n";
+
+            return message;
+        }
+
+    }
+}
---
+++ lw7/lw7/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lw7
+{
+    class Program
+    {
+        private const string MESSAGE_INCORRECT_AMOUNT_ARGUMENTS = "Incorrect amount arguments! Must will be ";
+        private const int AMOUNT_ARGUMENTS = 2;
+
+
+        static public void CheckParametrs(int argc)
+        {
+            if (argc != AMOUNT_ARGUMENTS)
+            {
+                throw new ArgumentException(MESSAGE_INCORRECT_AMOUNT_ARGUMENTS + AMOUNT_ARGUMENTS.ToString());
+            }
+        }
+
+        static int Main(string[] args)
+        {
+            try
+            {
+                //setlocale(LC_ALL, "RUS");
+
+                CheckParametrs(args.Length);
+
+                //boost::timer::cpu_timer timer;
+
+                ulong amountIteration = Convert.ToUInt64(args[0]);
+                ulong processesNumber = Convert.ToUInt64(args[1]);
+
+                //timer.start();
+                Console.WriteLine("{0}", CTaskSolver.GetPi(amountIteration, processesNumber));
+
+                //timer.stop();
+
+                //double time = timer.elapsed().wall * pow(10.f, -9.f);
+                //Console.WriteLine("time = {0}", time);
+
+                Console.ReadKey();
+                //system("pause");
+            }
+            catch (Exception except)
+            {
+                Console.WriteLine(except.ToString());
+                Console.ReadKey();
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+
+}

bd2809c852e5f0dc4bac6517882f6bf955addcde
Irina Yambatrova <yambatrova.irina@gmail.com>
2017-06-13 20:46:00 +0300

    Yambatrova Irina lw5 (1)

---
+++ lw5/Yambatrova_Irina(1)/5lab/5lab/CalculatePi.cpp
@@ -0,0 +1,53 @@
+#include "stdafx.h"
+#include <stdio.h>
+#include "CalculatePi.h"
+
+using namespace std;
+
+CalculatePi::CalculatePi()
+{
+}
+
+int RandInt(int max)
+{
+	return int(rand() % max);
+}
+
+float CalculatePiSequential(size_t iterationsCount)
+{
+	int rad = 1000;
+	int includedPoints = 0;
+	for (unsigned int i = 0; i < iterationsCount; ++i)
+	{
+		int x = RandInt(rad);
+		int y = RandInt(rad);
+		if (rad * rad >= x * x + y * y)
+		{
+			includedPoints++;
+		}
+	}
+	return float(4.f * (float(includedPoints) / float(iterationsCount)));
+}
+
+float CalculatePiParall(size_t iterationsCount)
+{
+	srand(unsigned int(time(0)));
+	int rad = 1000;
+	int includedPoints = 0;
+#pragma omp parallel num_threads(omp_get_num_procs()) 
+	{
+#pragma omp parallel  reduction (+:sum)
+		for (unsigned int i = 0; i < iterationsCount; ++i)
+		{
+			int x = RandInt(rad);
+			int y = RandInt(rad);
+			if (rad * rad >= x * x + y * y)
+			{
+				includedPoints++;
+			}
+		}
+	}
+	return float(4.f * (float(includedPoints) / float(iterationsCount)));
+}
+
+
---
+++ lw5/Yambatrova_Irina(1)/5lab/5lab/CalculatePi.h
@@ -0,0 +1,11 @@
+#pragma once
+class CalculatePi
+{
+public:
+	CalculatePi();
+	
+};
+
+int RandInt(int max);
+float CalculatePiSequential(size_t iterationsCount);
+float CalculatePiParall(size_t iterationsCount);

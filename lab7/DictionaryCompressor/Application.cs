0199b7dcab167851f1d0b83670c41c6e9594d379
Knyazev Sergey <yaknyazsergei@yandex.ru>
2017-06-13 16:39:55 +0300

    Добавлена lw7

---
+++ lab7/DictionaryCompressor/Application.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Threading;
+using System.Collections.Generic;
+
+namespace DictionaryCompressor
+{
+	class Application
+	{
+		private IOManager m_iomanager;
+		private RepositoryProcessedData m_myRepository;
+		private int m_nextThreadOrderForWrite;
+		private List<Thread> m_threads;
+		private Mutex m_mutex;
+		private Mutex m_mutexForCompressor;
+
+		public Application(IOManager manager)
+		{
+			m_iomanager = manager;
+			m_nextThreadOrderForWrite = 0;
+			m_myRepository = new RepositoryProcessedData();
+			m_threads = new List<Thread>();
+			m_mutex = new Mutex();
+			m_mutexForCompressor = new Mutex();
+		}
+
+		public void SetInputFileName(string name)
+		{
+			m_iomanager.SetInputFileName(name);
+		}
+		public void SetOutputFileName(string name)
+		{
+			m_iomanager.SetOutputFileName(name);
+		}
+
+		public void ProcessFile()
+		{
+			try
+			{
+				
+				while (!m_iomanager.IsFileCompletelyReadOut())
+				{
+					m_iomanager.SettingInputFile();
+					for (uint i = 0; i != Environment.ProcessorCount; ++i)
+					{
+						var thread = new Thread(ProcessNextFragment);
+						thread.Start();
+
+						m_threads.Add(thread);
+
+					}
+
+					foreach (var thread in m_threads)
+					{
+						if (thread.IsAlive)
+						{
+							thread.Join();
+						}
+					}
+
+					m_threads.RemoveRange(0, m_threads.Count);
+				}
+				System.Console.WriteLine("File processed");
+				return;
+			}
+			catch (Exception ex)
+			{
+			   System.Console.WriteLine("Error! " + ex.ToString());
+				return;
+			}
+		}
+
+		private void SaveNewInformation(Compressor compressor)
+		{
+			m_myRepository.SetNewTextFragment(compressor.GetAllProcessingText());
+		}
+
+		public void SaveResultsInFile()
+		{
+			m_iomanager.OutputDictionary(m_myRepository.GetDictionary());
+			m_iomanager.OutputProcessedText(m_myRepository.GetText());
+		}
+
+		private void ProcessNextFragment()
+		{
+			var success = true;
+			while (success)
+			{
+				try
+				{
+					m_mutex.WaitOne();
+					Compressor compressor = new Compressor(m_iomanager.GetOrder(), m_myRepository, m_mutexForCompressor);
+					var dataPtr = m_iomanager.GetViewMappingFile();
+					m_mutex.ReleaseMutex();
+					compressor.SetTextFragment(dataPtr);
+					compressor.SetLengthFragment(m_iomanager.GetSizeView());
+					compressor.EditFragment();
+
+					while (true)
+					{
+						if (compressor.GetOrder() == m_nextThreadOrderForWrite)
+						{
+							m_mutex.WaitOne();
+							SaveNewInformation(compressor);
+							m_mutex.ReleaseMutex();
+							++m_nextThreadOrderForWrite;
+							break;
+						}
+						else
+						{
+							Thread.Sleep(1000);
+						}
+					}
+				}
+				catch (Exception)
+				{
+					m_mutex.ReleaseMutex();
+					success = false;
+				}
+			}
+		}
+	}
+}
---
+++ lab7/DictionaryCompressor/IOManager.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace DictionaryCompressor
+{
+	class IOManager
+	{
+		private String m_inputFileName;
+		private String m_outputFileName;
+		private String m_dictionaryFileName;
+		private int m_innerCount;
+        private int m_roundMemoryMappingFile;
+        private bool m_isFileCompletelyReadOut;
+		private MemoryMappedFile m_memoryMappedFile;
+		private uint m_viewLength;
+		private long m_remainderLengthOfFileNotRead;
+		private long m_remainderLengthOfReadFragment;
+		
+		public IOManager(string inputFileName, string outputFileName)
+		{
+			m_inputFileName = inputFileName;
+			m_outputFileName = outputFileName;
+			m_dictionaryFileName = "dictionary.txt";
+			m_innerCount = 0;
+			m_roundMemoryMappingFile = 0;
+			m_isFileCompletelyReadOut = false;
+			m_memoryMappedFile = null;
+		}
+
+		public int GetOrder()
+		{
+			return m_innerCount;
+		}
+
+		public void OutputDictionary(Dictionary<string, string> dictionary)
+		{
+			using (StreamWriter sw = new StreamWriter(m_dictionaryFileName, false, System.Text.Encoding.Default))
+			{
+				foreach (var item in dictionary)
+				{
+					sw.WriteLine(item.Key + ":" + item.Value);
+				}
+			}
+		}
+
+		public void OutputProcessedText(string text)
+		{
+			using (StreamWriter sw = new StreamWriter(m_outputFileName, false, System.Text.Encoding.Default))
+			{
+				sw.WriteLine(text);
+			}
+		}
+
+		public void SetInputFileName(string name)
+		{
+			m_inputFileName = name;
+		}
+
+		public void SetOutputFileName(string name)
+		{
+			m_outputFileName = name;
+		}
+
+		public uint GetSizeView()
+		{
+			return m_viewLength;
+		}
+
+		public bool IsFileCompletelyReadOut()
+		{
+			return m_isFileCompletelyReadOut;
+		}
+
+		public void SettingInputFile()
+		{
+			try
+			{
+				CreateMemoryMappingFile();
+			}
+			catch(Exception ex)
+			{
+				throw ex;
+			}
+		}
+
+		public string GetViewMappingFile()
+		{
+			try
+			{
+				if (m_remainderLengthOfReadFragment < 0)
+				{
+					throw new TaskCanceledException("File ended!");
+				}
+				int allocationGranularity = 65536;
+				string data;
+				if (m_remainderLengthOfReadFragment < allocationGranularity)
+				{
+					data = GetContent(allocationGranularity * m_innerCount++, m_remainderLengthOfReadFragment);
+					m_viewLength = (uint)m_remainderLengthOfReadFragment;
+					m_isFileCompletelyReadOut = true;
+				}
+				else
+				{
+					data = GetContent(allocationGranularity * m_innerCount++, allocationGranularity);
+					m_viewLength = (uint)allocationGranularity;
+				}
+				m_remainderLengthOfReadFragment -= allocationGranularity;
+				System.Console.WriteLine("Received data fragment");
+				return data;
+			}
+			catch(TaskCanceledException ex)
+			{
+				throw ex;
+			}
+			catch (UnauthorizedAccessException ex)
+			{
+				Console.WriteLine("Cannot create View Streem in mapping memory file");
+				throw ex;
+			}
+		}
+
+		private void CreateMemoryMappingFile()
+		{
+			try
+			{
+			   
+				m_remainderLengthOfFileNotRead = new System.IO.FileInfo(m_inputFileName).Length;
+
+				m_remainderLengthOfReadFragment = m_remainderLengthOfFileNotRead;
+				ulong availPhys = 0;
+				MEMORYSTATUSEX memStatus = new MEMORYSTATUSEX();
+				GlobalMemoryStatusEx(memStatus);
+				availPhys = memStatus.ullTotalPhys;
+				ulong twentyPercentOfAvailPhys = availPhys / 5;
+				long size = 0;
+				if ((ulong)m_remainderLengthOfReadFragment < twentyPercentOfAvailPhys)
+				{
+					size = m_remainderLengthOfReadFragment;
+					m_remainderLengthOfFileNotRead = 0;
+					
+				}
+				else
+				{
+					size = (long)twentyPercentOfAvailPhys;
+					m_remainderLengthOfFileNotRead -= (long)twentyPercentOfAvailPhys;
+				}
+				m_memoryMappedFile = MemoryMappedFile.CreateFromFile(m_inputFileName, FileMode.Open, "FileForMapping", size);
+			}
+			catch (ArgumentException ex)
+			{
+				System.Console.WriteLine("Argument exception: " + ex.ToString());
+				throw ex;
+			}
+			catch (IOException ex)
+			{
+				System.Console.WriteLine("Error: " + ex.ToString());
+				throw ex;
+			}
+		}
+
+		private string GetContent(long beginningByteLocation, long bytesToReadIn)
+		{
+			try
+			{ 
+				string content;
+
+				using (var memoryMappedViewStream = m_memoryMappedFile.CreateViewStream(beginningByteLocation, bytesToReadIn, MemoryMappedFileAccess.Read))
+				{
+					var contentArray = new byte[bytesToReadIn];
+					memoryMappedViewStream.Read(contentArray, 0, contentArray.Length);
+					content = Encoding.UTF8.GetString(contentArray);
+				}
+
+				return content;
+			}
+			catch(UnauthorizedAccessException ex)
+			{
+				throw ex;
+			}
+		}
+
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+		private class MEMORYSTATUSEX
+		{
+			public uint dwLength;
+			public uint dwMemoryLoad;
+			public ulong ullTotalPhys;
+			public ulong ullAvailPhys;
+			public ulong ullTotalPageFile;
+			public ulong ullAvailPageFile;
+			public ulong ullTotalVirtual;
+			public ulong ullAvailVirtual;
+			public ulong ullAvailExtendedVirtual;
+			public MEMORYSTATUSEX()
+			{
+				this.dwLength = (uint)Marshal.SizeOf(typeof(MEMORYSTATUSEX));
+			}
+		}
+
+		[return: MarshalAs(UnmanagedType.Bool)]
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		static extern bool GlobalMemoryStatusEx([In, Out] MEMORYSTATUSEX lpBuffer);
+
+	}
+}

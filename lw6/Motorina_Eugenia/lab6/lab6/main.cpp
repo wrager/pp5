b276e4f31943e9303c793bad678cfc4337987069
Stepan <Cogucc@mail.ru>
2017-06-13 16:06:54 +0300

    Добавлена считка массива из файла

--- lw6/Motorina_Eugenia/lab6/lab6/main.cpp
+++ lw6/Motorina_Eugenia/lab6/lab6/main.cpp
@@ -1,14 +1,20 @@
 #include "stdafx.h"
 #include "Sorter.h"
-
+#include <fstream>
 
 int main()
 {
 	CSorter sorter;
-	std::vector<int> a = {9, 8, 6, 4, 7, 2, 3, 1, 54};
-	//sorter.MergeSort(a, 0, a.size() - 1);
-	sorter.SortWithThreads(a, 4);
-	for (auto &it : a)
+	std::ifstream input("input.txt");
+	std::vector<int> arr;
+	while (!input.eof())
+	{
+		int num;
+		input >> num;
+		arr.push_back(num);
+	}
+	sorter.SortWithThreads(arr, 4);
+	for (auto &it : arr)
 	{
 		std::cout << it << ", ";
 	}

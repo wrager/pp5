2b6db24a06a35b6c326452af515ac9dab879085e
Alexey Ildarkin <ildarkin.alexey@gmail.com>
2017-06-16 09:18:20 +0300

    Ildarkin Alexey lw7 (2)
    * lw7 (pp)
    
    * Поправил местонахождение lw7

---
+++ lw7/2/Ildarkin_Alexey/ArraySorts_Sharp/Utils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySorts_Sharp
+{
+    class Utils
+    {
+        public static List<int> ReadFileToVec(string filename)
+        {
+            List<int> numbers = File.ReadAllText(filename)
+                .Split(new char[] { '\t', ' ', '\n' }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.All(char.IsDigit))
+                .Select(int.Parse)
+                .ToList();
+
+            return numbers;
+        }
+
+        public static double NotifyExecutionTime(Action callback)
+        {
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+            callback();
+            watch.Stop();
+            return watch.ElapsedMilliseconds;
+        }
+    }
+}
